3d8e49f0160481bd2e39efdfcbef239f52c1cf1e
Robert <ishbuldinrobert2002@gmail.com>
2023-05-29 11:51:58 +0500

    ex36 and ex38 done

--- ex36/Program.cs
+++ ex36/Program.cs
@@ -3,3 +3,42 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int[] mass = new int[5];
+
+void FillArr(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = new Random().Next(-25, 26);
+    }
+}
+
+void PrintArr(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (i == 0) Console.Write($"[{mass[i]}, ");
+        else if (i == mass.Length - 1) Console.WriteLine($"{mass[i]}]");
+        else Console.Write($"{mass[i]}, ");
+    }
+}
+
+int FindSum(int[] mass)
+{
+    int i = 0;
+    int sum = 0;
+    while (i < mass.Length)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + mass[i];
+        }
+        i++;
+    }
+    return sum;
+}
+
+FillArr(mass);
+PrintArr(mass);
+
+Console.WriteLine($"Сумма элементов на нечетных позициях: {FindSum(mass)}");
---
+++ ex38/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 38:
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] mass = new int[7];
+
+void FillArr(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = new Random().Next(-25, 26);
+    }
+}
+
+void PrintArr(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (i == 0) Console.Write($"[{mass[i]}, ");
+        else if (i == mass.Length - 1) Console.WriteLine($"{mass[i]}]");
+        else Console.Write($"{mass[i]}, ");
+    }
+}
+
+FillArr(mass);
+PrintArr(mass);
+
+int i = 0;
+int max = mass[i];
+int min = mass[i];
+
+while (i < mass.Length)
+{
+    if (mass[i] > max)
+    {
+        max = mass[i];
+    }
+    else if (mass[i] < min)
+    {
+        min = mass[i];
+    }
+    i++;
+}
+Console.WriteLine($"Разница между максимальным и минимальныи элементами: {max - min}");
